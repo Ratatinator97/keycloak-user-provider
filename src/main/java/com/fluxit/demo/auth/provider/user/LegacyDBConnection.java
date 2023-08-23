9d72bed4a55bc177f7ead15954a86d0657b30179
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-08-23 17:10:26 +0200

    Corrected some bugs

--- src/main/java/com/fluxit/demo/auth/provider/user/LegacyDBConnection.java
+++ src/main/java/com/fluxit/demo/auth/provider/user/LegacyDBConnection.java
@@ -7,13 +7,13 @@ import java.sql.DriverManager;
 
 public class LegacyDBConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/keycloak";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String CONFIG_KEY_JDBC_URL = "config.key.jdbc.url";
+    private static final String CONFIG_KEY_DB_USERNAME = "config.key.db.username";
+    private static final String CONFIG_KEY_DB_PASSWORD = "config.key.db.password";
 
-    public static Connection getConnection() {
+    public static Connection getConnection(ComponentModel config) {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(config.get(CONFIG_KEY_JDBC_URL), config.get(CONFIG_KEY_DB_USERNAME), config.get(CONFIG_KEY_DB_PASSWORD));
         } catch (Exception e) {
             e.printStackTrace();
             return null;
